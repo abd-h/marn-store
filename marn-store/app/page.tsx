db4c3c92f0f95da45796d749e52830873a3cdcc8
Abdalla Hussein <a.hussein1934@gmail.com>
2025-10-29 23:53:04 +0000

    test complete

--- marn-store/app/page.tsx
+++ marn-store/app/page.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
+import TopBar from "../components/navigations/TopBar";
+import MainNav from "../components/navigations/MainNav";
+import SearchBanner from "@/components/icons/nav/search/SearchBanner";
+import OverlayLayer from "@/components/icons/nav/search/OverlayLayer";
+
+
+
+
 export default function Home() {
   return (
-    <section className="p-8 text-xl">
-      <h1>✅ Marn.co.uk is live!</h1>
-      <p>This is a test homepage.</p>
-    </section>
+    <main className="contents">
+      <MainNav />
+      <OverlayLayer />
+      <div className="w-[40rem] outline mx-auto flex flex-col gap-2 py-8 mt-[10%] items-center justify-center">
+        <h1 className="text-2xl text-center my-[20%]">Marn.co.uk</h1>
+        <p>
+          This is the home page and It&apos;s your store were main content would
+          go
+        </p>
+      </div>
+      ;
+    </main>
   );
 }
